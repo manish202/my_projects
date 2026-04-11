8d0555dc8199b32fd861feb05b6dbd0d4a65c374
manish202 <godofmind51616@gmail.com>
2026-04-11 15:06:42 +0530

    created separate card component

--- src/components/ProjectsSection.jsx
+++ src/components/ProjectsSection.jsx
@@ -1,25 +1,6 @@
 import {useState,useEffect} from 'react';
 import projects from '../static_assets/projects';
-
-const Card = ({id,title,description,thumbnail,approx_date,keywords,source_code,live_preview,highlighted,private_repo}) => {
-    const [view, setView] = useState(false);
-    return (
-        <div className='card' title={id}>
-            <div className="thumbnail"><img src={`images/projects/${thumbnail}`} alt={title} /></div>
-            <h2>{title}</h2>
-            <h3>({approx_date})</h3>
-            <div className={`desc ${view ? 'active':''}`}>
-                <p>{description}</p>
-                <p>Tech: {keywords.join(', ')}</p>
-            </div>
-            <div className='d-flex btn-box'>
-                <button type='button' className='btn view' onClick={() => setView(o => !o)}>{view ? 'close':'view'}</button>
-                <a href={source_code} className='btn source' target='_blank'>{private_repo ? "Private Repo":"Source Code"}</a>
-                {live_preview && <a href={live_preview} className='btn live' target='_blank'>Live Preview</a>}
-            </div>
-        </div>
-    )
-}
+import Card from './Card';
 
 const handleCalculation = (state) => {
     const sort_by = state.sort_by ?? 'L_first';
